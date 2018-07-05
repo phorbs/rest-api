5ba1acae3f87a307bf5c0ef78c60e055493e452c
Emanuel Santos <phorbs@gmail.com>
2018-07-05 15:08:30 +0100

    update

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 var config = {
     database: {
+        host: 'infopower.pt.mysql', 		// host da Base de Dados
+        user: 'infopower_pt', 			// username da Base de Dados
+        password: 'EmanuelComedorDeGatas', 			// password da Base de Dados
+        port: 3306, 			// MySQL porto por defeito
+        database: 'infopower_pt' 	// nome da base de dados
+    },
+/*     database: {
         host: 'localhost', 		// host da Base de Dados
         user: 'root', 			// username da Base de Dados
         password: 'emanuel', 			// password da Base de Dados
         port: 3306, 			// MySQL porto por defeito
         database: 'infopower_pt' 	// nome da base de dados
-    },
+    }, */
     server: {
-        host: '127.0.0.1',
+        host: 'https://guarded-temple-64733.herokuapp.com/',
         port: '8888'
     },
     secret: 'supersecret'
